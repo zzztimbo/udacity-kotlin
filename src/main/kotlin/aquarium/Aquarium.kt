997538b42a7026b8b4e2ac12878ac4273ff0c46e
Tim Chan <tim@chan.net>
2022-01-16 17:29:11 -0800

    add setter for volume

--- src/main/kotlin/aquarium/Aquarium.kt
+++ src/main/kotlin/aquarium/Aquarium.kt
@@ -5,8 +5,9 @@ class Aquarium {
     var height: Int = 40
     var length: Int = 100
 
-    val volume: Int
+    var volume: Int
       get() =  width * height * length / 1000
+      set(value) { height = (value * 1000) / (width * length) }
 
 
 }
